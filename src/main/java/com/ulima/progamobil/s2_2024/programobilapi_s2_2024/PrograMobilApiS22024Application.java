41a712af5fee1a2bdc19b5e02b6d57b49c6936a3
DarkShadow76 <alonzotoricaceres@gmail.com>
2024-09-01 00:08:02 -0500

    first commit

---
+++ src/main/java/com/ulima/progamobil/s2_2024/programobilapi_s2_2024/PrograMobilApiS22024Application.java
@@ -0,0 +1,13 @@
+package com.ulima.progamobil.s2_2024.programobilapi_s2_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrograMobilApiS22024Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PrograMobilApiS22024Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/ulima/progamobil/s2_2024/programobilapi_s2_2024/PrograMobilApiS22024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulima.progamobil.s2_2024.programobilapi_s2_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrograMobilApiS22024ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
